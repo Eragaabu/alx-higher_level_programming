eb3e1ff87a790666c1ab10605b1b052ff17e29ee
Eragaabu <eragaabu@gmail.com>
2022-09-20 20:53:18 +0000

    corrected done

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-for num in range(0, 90):
-        if num % 10 > num / 10:
-                if num != 89:
-                        print("{:02d}, ".format(num), end='')
-                else:
-                        print("{:02d}".format(num))
+
+for num in range(0, 9):
+    for num_1 in range(1, 10):
+        if num < num_1:
+            if num == 8:
+                print("{}{}".format(num, num_1))
+            else:
+                print("{}{},".format(num, num_1), end=" ")
